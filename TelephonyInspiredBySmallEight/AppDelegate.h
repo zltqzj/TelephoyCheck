cd3e119dc955ef479d7fca4b4b435fa6b9cff97c
zhaojian <zltqzj@163.com>
2015-12-08 14:28:12 +0800

    拨打电话，接电话等状态的检测。

---
+++ TelephonyInspiredBySmallEight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TelephonyInspiredBySmallEight
+//
+//  Created by zkr on 12/8/15.
+//  Copyright © 2015 zkr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TelephonyInspiredBySmallEight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TelephonyInspiredBySmallEight
+//
+//  Created by zkr on 12/8/15.
+//  Copyright © 2015 zkr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
